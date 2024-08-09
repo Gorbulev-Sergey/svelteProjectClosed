dd57386e582931a1549c6d0702b7e298986ed0f6
Gotbulev-Sergey <gorbulevsv@yandex.ru>
2024-08-09 18:16:46 +0300

    1

---
+++ src/routes/driver/orders/+page.svelte
@@ -0,0 +1,5 @@
+<svelte:head><title>Проект закрыт!</title></svelte:head>
+
+<div class="d-flex align-items-center justify-content-center" style="height: 100vh;">
+	<b class="display-1 text-dark text-center">Проект закрыт!</b>
+</div>
